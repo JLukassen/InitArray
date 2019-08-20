65bea4872af59eee379f36c3c0b1dbca51991989
yitakumini <30408410+yitakumini@users.noreply.github.com>
2019-08-20 18:05:51 -0400

    Add project files.

---
+++ InitArray/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class InitArray
+{
+  
+        static void Main(string[] args)
+        {
+            
+            int[] array = { 32, 27, 64, 18, 95, 14, 90, 70, 60, 37 };
+
+            Console.WriteLine("{0}{1,8}", "Index", "Value");
+
+            for (int counter = 0; counter < array.Length; ++counter)
+                Console.WriteLine("{0,5}{1,8}", counter, array[counter]);
+        }
+    
+}
